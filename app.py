ac5647182984d8dc4fa24f3f61d4ce9ed20bc9f6
saufiputra52-alt <saufiputra52@gmail.com>
2026-09-07 22:39:37 +0700

    Create app.py
    echo "# dataku-desa" >> README.md
    git init
    git add README.md
    git commit -m "first commit"
    git branch -M main
    git remote add origin https://github.com/saufiputra52-alt/dataku-desa.git
    git push -u origin main
    git remote add origin https://github.com/saufiputra52-alt/dataku-desa.git
    git branch -M main
    git push -u origin main

---
+++ app.py
@@ -0,0 +1,10 @@
+echo "# dataku-desa" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/saufiputra52-alt/dataku-desa.git
+git push -u origin main
+git remote add origin https://github.com/saufiputra52-alt/dataku-desa.git
+git branch -M main
+git push -u origin main
